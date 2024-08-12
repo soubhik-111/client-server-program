297d635187bd9b93930a62ccdcbd5576e2bd714c
Soubhik <crazy@kali>
2024-08-12 16:28:53 +0530

    Request fallback

--- client/src/request.js
+++ client/src/request.js
@@ -2,12 +2,9 @@ import axios from 'axios';
 import { servers } from './constants';
 
 let currentServerIndex = 0;
+const requestData = "hi";
 
 const sendRequest = async (timeStamp) => {
-    const currentServer = servers[currentServerIndex];
-    const requestData = "hi";
-    const requestURL = currentServer+requestData;
-
     let res = {
         timeStamp, 
         requestData, 
@@ -15,20 +12,39 @@ const sendRequest = async (timeStamp) => {
         response: '-'
     }
 
-    try {    
-        const response = await axios.get(requestURL, requestData);
-        console.log(`Response from ${requestURL}: ${response.data}`);
+    try {
+        // Attempt to send a request to the current server
+        const currentServer = servers[currentServerIndex];
+        const requestURL = currentServer + requestData;
+        const response = await axios.get(requestURL);
 
-        
-        res = { ...res, response: response.data }
+        console.log(`Response from ${requestURL}: ${response.data}`);
+        res = { ...res, response: response.data };
+
+        // Only advance the server index if the request was successful
+        currentServerIndex = (currentServerIndex + 1) % servers.length;
     } catch (error) {
-        console.error(`Error communicating with server: ${error.message}`);
+        console.error(`Error communicating with ${servers[currentServerIndex]}: ${error.message}`);
+
+        // Attempt to send a request to the other server if the current one fails
+        const fallbackServerIndex = (currentServerIndex + 1) % servers.length;
+        try {
+            const fallbackServer = servers[fallbackServerIndex];
+            const requestURL = fallbackServer + requestData;
+            const response = await axios.get(requestURL);
+
+            console.log(`Response from ${requestURL}: ${response.data}`);
+            res = { ...res,server: `${res.server}>` , response: response.data };
+
+            // If the fallback server succeeds, set the current index to the fallback server
+            currentServerIndex = fallbackServerIndex;
+        } catch (err) {
+            currentServerIndex = (currentServerIndex + 1) % servers.length;
+            console.error(`Error communicating with both servers: ${err.message}`);
+        }
     }
-    
-    // Move to the next server in the round-robin
-    currentServerIndex = (currentServerIndex + 1) % servers.length;
+
     return res;
-    
 };
 
 export default sendRequest;
